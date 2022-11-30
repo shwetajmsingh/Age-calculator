9946f6007cad7c2b91392d222187282491ff5c0e
Shweta Singh <singhshweta1916786@gmail.com>
2022-11-30 18:18:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+date = document.querySelector('date');
+       month = document.querySelector('month');
+       year = document.querySelector('year');
+       submit = document.querySelector('submit');
+        
+      let result=0;
+
+       for(let i=0 ; i<year ; i++){
+        if(year<2023){
+             result= 2023-querySelector('year');
+        }
+        return result;
+       }
